c9bca56f1093c0189fb87d11d73b95647a113d5a
KaHooli <james@cavanaghs.id.au>
2017-03-21 10:47:55 +1000

    Updating MP4 Automator dependencies as well

--- init/01_update_mp4automator.sh
+++ init/01_update_mp4automator.sh
@@ -2,3 +2,13 @@
 git -C /scripts/MP4_Automator/ reset HEAD --hard
 git -C /scripts/MP4_Automator/ pull
 chmod 775 -R /scripts
+pip install --upgrade PIP
+pip install --upgrade requests
+pip install --upgrade requests[security]
+pip install --upgrade requests-cache
+pip install --upgrade babelfish
+pip install --upgrade "guessit<2"
+pip install --upgrade "subliminal<2"
+pip install --upgrade qtfaststart
+pip uninstall stevedore
+install stevedore==1.19.1
