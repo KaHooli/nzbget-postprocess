c49a01dffb03867ec196678f2ac8fea9321f21d1
KaHooli <james@cavanaghs.id.au>
2018-03-24 14:41:18 +1000

    Updating nzbToMedia and remove owner
    1. Updating nzbToMedia script files
    2. Removes owner of script files, as root owner was causing issue with executing script from NZBget.

--- init/01_update.sh
+++ init/01_update.sh
@@ -3,7 +3,10 @@ apk update
 apk upgrade
 git -C /scripts/MP4_Automator/ reset HEAD --hard
 git -C /scripts/MP4_Automator/ pull
-chmod 775 -R /scripts
+git -C /scripts/nzbToMedia/ reset HEAD --hard
+git -C /scripts/nzbToMedia/ pull
+chmod -R 0777 /scripts
+chown -R nobody:nogroup /scripts
 pip install --upgrade PIP
 pip install --upgrade requests
 pip install --upgrade requests[security]
@@ -11,6 +14,7 @@ pip install --upgrade requests-cache
 pip install --upgrade babelfish
 pip install --upgrade "guessit<2"
 pip install --upgrade "subliminal<2"
+pip install --upgrade python-dateutil
 pip install --upgrade qtfaststart
 pip uninstall --yes stevedore
 pip install stevedore==1.19.1
